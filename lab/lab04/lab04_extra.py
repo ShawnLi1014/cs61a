e5bfab1f2171d825af725b15d1a7e4b8fde18591
ShawnLi1014 <shawnli1014@gmail.com>
2018-02-19 21:06:08 +0800

    lab04 finished

---
+++ lab/lab04/lab04_extra.py
@@ -0,0 +1,44 @@
+from lab04 import *
+
+# Q13
+def flatten(lst):
+    """Returns a flattened version of lst.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    """
+    "*** YOUR CODE HERE ***"
+    s = []
+    for x in lst:
+        if type(x) == list:
+            s = s + flatten(x)
+        else:
+            s = s + [x]
+    return s
+
+# Q14
+def merge(lst1, lst2):
+    """Merges two sorted lists.
+
+    >>> merge([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    "*** YOUR CODE HERE ***"
+    if not lst1 or not lst2:
+        return lst1 + lst2
+    elif lst1[0] < lst2[0]:
+        return [lst1[0]] + merge(lst1[1:], lst2)
+    else:
+        return [lst2[0]] + merge(lst1, lst2[1:])
